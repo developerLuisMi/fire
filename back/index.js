77bc1889789548606822f09a18817171a6dbb505
developerLuisMi <desing.web.lm@gmail.com>
2024-01-22 19:43:20 -0500

    modificado fire

--- back/index.js
+++ back/index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3001;
 
 app.get("/", function (req, res) {
-  res.send("hola mundo probando el proyecto vite con desplegue");
+  res.send(
+    "hola mundo probando el proyecto vite con desplegue cambio en el servidor para fire"
+  );
 });
 
 app.listen(port, function () {
