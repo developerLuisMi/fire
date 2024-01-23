d521e8a51dcebd6473bc0fb570fd8343095399dd
developerLuisMi <desing.web.lm@gmail.com>
2024-01-22 19:30:33 -0500

    subiendo a fire

---
+++ back/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.get("/", function (req, res) {
+  res.send("hola mundo probando el proyecto vite con desplegue");
+});
+
+app.listen(port, function () {
+  console.log(`Servidor Express escuchando en http://localhost:${port}`);
+});
